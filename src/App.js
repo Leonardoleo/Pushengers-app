30cdda5d4ebcb650569c1e5120a0c8c51166b8da
Leonardoleo <leo89_my@hotmail.com>
2020-07-08 00:25:21 +0800

    update

--- src/App.js
+++ src/App.js
@@ -13,15 +13,14 @@ function App() {
         alt = "logo" / >
         <
         p >
-        Edit < code > src / App.js < /code> and save to reload. </p >
-        <
+        Edit < code > src / App.js < /code> and save to reload.  <
+        /p> <
         a className = "App-link"
         href = "https://reactjs.org"
         target = "_blank"
         rel = "noopener noreferrer" >
-        Hello World!
-        <
-        /a>   <
+        Pushengers <
+        /a>    <
         /header>  <
         /div>
     );
