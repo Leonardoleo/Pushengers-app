8878fdce80943ab8b77821c2958becacf432dc20
Leonardoleo <leo89_my@hotmail.com>
2020-07-08 15:11:58 +0800

    deleted and modified

---
+++ src/components/Business/Business.js
@@ -0,0 +1,11 @@
+const business = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+};
