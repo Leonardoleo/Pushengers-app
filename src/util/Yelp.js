169540bb51ae1eb4447c5d0bdd2430505087f6aa
Leonardoleo <leo89_my@hotmail.com>
2020-07-12 20:08:44 +0800

    Interacting with Yelp API

---
+++ src/util/Yelp.js
@@ -0,0 +1,33 @@
+const apiKey = '';
+
+const yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+        }).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(((business) => {
+                    console.log(business);
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count,
+                    };
+                }));
+            }
+        })
+    }
+};
+
+export default yelp;
