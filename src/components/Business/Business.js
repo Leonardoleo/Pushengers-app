500b463a542f43ec6e1fcbbcd7c7826a012d1de5
Leonardoleo <leo89_my@hotmail.com>
2020-07-21 23:25:42 +0800

    Allow you to search by pressing Enter and Implment user type of sort, by entering a distance from a central location

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -19,16 +19,16 @@ class Business extends React.Component {
             <
             p > { this.props.business.address } < /p> <
             p > { this.props.business.city } < /p> <
-            p > { `${this.props.business.state} ${this.props.business.zipCode}` } < /p> <
-            /div> <
+            p > { `${this.props.business.state} ${this.props.business.zipCode}` } < /p> < /
+            div > <
             div className = "Business-reviews" >
             <
             h3 > { this.props.business.category.toUpperCase() } < /h3> <
             h3 className = "rating" > { `${this.props.business.rating} stars` } < /h3> <
-            p > { `${this.props.business.reviewCount} reviews` } < /p> <
-            /div> <
-            /div> <
-            /div>
+            p > { `${this.props.business.reviewCount} reviews` } < /p> < /
+            div > <
+            /div> < /
+            div >
         );
     }
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,101 +2,113 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
-        constructor(props) {
-            super(props);
+    constructor(props) {
+        super(props);
 
-            this.state = {
-                term: '',
-                location: '',
-                sortBy: 'best_match'
-            };
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        };
 
-            this.handleTermChange = this.handleTermChange.bind(this);
-            this.handleLocationChange = this.handleLocationChange.bind(this);
-            this.handleSearch = this.handleSearch.bind(this);
-            this.handleSortByChange = this.handleSortByChange.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleSortByChange = this.handleSortByChange.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
 
-            this.sortByOptions = {
-                'Best Match': 'best_match',
-                'Highest Rated': 'rating',
-                'Most Reviewed': 'review_count',
-                'Distance From': 'distance'
-            };
+        this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count',
+            'Distance From': 'distance'
+        };
+    }
+
+    getSortByClass(sortByOption) {
+        if (this.state.sortBy === sortByOption) {
+            return 'active';
         }
+        return '';
+    }
 
-        getSortByClass(sortByOption) {
-            if (this.state.sortBy === sortByOption) {
-                return 'active';
-            }
-            return '';
-        }
-
-        handleSortByChange(sortByOption) {
-            this.setState({
-                sortBy: sortByOption
-            }, () => {
-                this.props.searchYelp(this.state.term, this.state.location, this.location.sortBy);
+    handleSortByChange(sortByOption) {
+        this.setState({ sortBy: sortByOption },
+            () => {
+                if (this.state.term && this.state.location) {
+                    this.props.searchYelp(this.state.term, this.state.location,
+                        this.state.sortBy);
+                }
             });
-        }
+    }
 
-        handleTermChange(event) {
-            this.setState({ term: event.target.value });
-        }
+    handleTermChange(event) {
+        this.setState({ term: event.target.value });
+    }
 
-        handleLocationChange(event) {
-            this.setState({ location: event.target.value });
-        }
+    handleLocationChange(event) {
+        this.setState({ location: event.target.value });
+    }
 
-        handleSearch(event) {
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        event.preventDefault();
+    }
+
+    handleKeyPress(event) {
+        if (event.charCode === 13) {
             this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
-
             event.preventDefault();
         }
+    };
 
-        handleKeyPress(event) {
-            if (event.key === 'Enter') {
-                this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
-            }
-        }
+    renderSortByOptions() {
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return ( <
+                li className = { this.getSortByClass(sortByOptionValue) }
+                key = { sortByOptionValue }
+                onClick = { this.handleSortByChange.bind(this, sortByOptionValue) } > { sortByOption } <
+                /li>
+            );
+        });
+    }
 
-        renderSortByOptions() {
-            return Object.keys(this.sortByOptions).map(sortByOption => {
-                    let sortByOptionValue = this.sortByOptions[sortByOption];
-                    return ( < li className = { this.getSortByClass(sortByOptionValue) }
-                        key = { sortByOptionValue }
-                        onClick = { this.handleSortByChange.bind(this, sortByOptionValue) } > { sortByOption } <
-                        /li>);
-                    });
-            }
+    render() {
+        return ( <
+            div className = "SearchBar" >
+            <
+            div className = "SearchBar-sort-options" >
+            <
+            ul > { this.renderSortByOptions() } <
+            /ul>   <
+            /div> 
 
-            render() {
-                return ( <
-                    div className = "SearchBar" >
-                    <
-                    div className = "SearchBar-sort-options" >
-                    <
-                    ul > { this.renderSortByOptions() } <
-                    /ul> < /
-                    div > <
-                    div className = "SearchBar-fields" >
-                    <
-                    input placeholder = "Search Businesses"
-                    onChange = { this.handleTermChange }
-                    onKeyPress = { this.handleKeyPress }
-                    /> <
-                    input placeholder = "Where?"
-                    onChange = { this.handleLocationChange }
-                    onKeyPress = { this.handleKeyPress }
-                    /> < /
-                    div > <
-                    div className = "SearchBar-submit" >
+            <
+            div className = "SearchBar-fields" >
 
-                    <
-                    a onClick = { this.handleSearch } > Let 's Go</a> < /
-                    div > <
-                    /div>
-                );
-            }
-        }
+            <
+            input onKeyPress = { this.handleKeyPress }
+            onChange = { this.handleTermChange }
+            placeholder = "Search Businesses" /
+            >
 
-        export default SearchBar;
+            <
+            input onKeyPress = { this.handleKeyPress }
+            onChange = { this.handleLocationChange }
+            placeholder = "Where?" /
+            >
+
+            <
+            /div>   <
+            div onClick = { this.handleSearch }
+            className = "SearchBar-submit" >
+            <
+            a > Let 's Go</a> <
+            /div>  <
+            /div>
+        );
+    }
+}
+
+export default SearchBar;
